c6adee728eaa7d1fe13e3231035f807986bac8f5
habil99 <abiyevhabil3@gmail.com>
2021-09-17 12:27:48 +0400

    Complete responsiveness of product detail page

--- js/product-carousel.js
+++ js/product-carousel.js
@@ -1,19 +1,39 @@
 const product_carousel = document.getElementsByClassName("product-view__swiper")[0];
 const suggestedProductsC = document.getElementsByClassName("suggested-products-carousel")[0];
 
-const swiper = new Swiper(product_carousel, {
-  speed: 1200,
-  direction: 'vertical',
-  slidesPerView: 'auto',
-  spaceBetween: 10,
-  freeMode: true,
-  breakpoints: {
-    1200: {
-      slidesPerView: 'auto',
-      spaceBetween: 10
-    }
+let desktop = true;
+let dir = 'vertical'
+
+determineDeviceW()
+window.onresize = () => determineDeviceW()
+
+function determineDeviceW() {
+  if (window.innerWidth > 768) {
+    desktop = true
+    setSwiper(desktop)
+  } else {
+    desktop = false
+    setSwiper(desktop)
   }
-})
+}
+
+setSwiper(desktop)
+
+function setSwiper(desktop) {
+  new Swiper(product_carousel, {
+    speed: 1200,
+    direction: desktop === true ? 'vertical' : 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    freeMode: true,
+    breakpoints: {
+      1200: {
+        slidesPerView: 'auto',
+        spaceBetween: 10
+      }
+    }
+  })
+}
 
 const suggestedProductsCaorusel = new Swiper(suggestedProductsC, {
   speed: 1200,
